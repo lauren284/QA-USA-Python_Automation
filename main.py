d79cfc9e9e9e3e9e4160b82dd7398d0bc7b9a8b9
lauren284 <LMBrewster84@msn.com>
2025-02-09 14:17:45 -0500

    main.py

--- main.py
+++ main.py
@@ -1,51 +1,107 @@
+import time
+from selenium import webdriver
 import data
 import helpers
+from pages import UrbanRoutesPage
+
 class TestUrbanRoutes:
     @classmethod
-    def setup_class(cls):
-        if helpers.is_url_reachable(data.URBAN_ROUTES_URL)
-            print("Connected to the Urban Routes server")
-        else:
-            print("Cannot connect to Urban Routes. Check the server is on and still running")
+def setup_class(cls):
+    from selenium.webdriver import DesiredCapabilities
+    capabilities = DesiredCapabilities.CHROME
+    capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+    cls.driver = webdriver.Chrome()
+    if helpers.is_url_reachable(data.URBAN_ROUTES_URL)
+        print("Connected to the Urban Routes server")
+    else:
+        print("Cannot connect to Urban Routes. Check the server is on and still running")
 def test_set_route(self):
-# Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_call_a_taxi_button()
+    actual_value = urban_routes_page.check_taxi_selector()
+    expected_value = 'Call a taxi'
+    assert actual_value == expected_value
 
 def test_select_plan(self):
-#Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_custom_option()
+    urban_routes_page.click_taxi_option()
+    urban_routes_page.click_call_taxi_button()
+    urban_routes_page.click_supportive_option()
+    time.sleep(2)
+    assert urban_routes_page.get_supportive_option()
+
 
 def test_fill_phone_number(self):
-#Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_custom_option()
+    urban_routes_page.click_taxi_option()
+    urban_routes_page.click_call_taxi_button()
+    urban_routes_page.click_supportive_option()
+    time.sleep(2)
+    assert urban_routes_page.get_supportive_option()
 
 def test_fill_card(self):
-#Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_custom_option()
+    urban_routes_page.click_taxi_option()
+    urban_routes_page.click_call_taxi_button()
+    urban_routes_page.click_supportive_option()
+    time.sleep(2)
+    assert urban_routes_page.get_supportive_option()
 
 def test_comment_for_driver(self):
-#Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_custom_option()
+    urban_routes_page.click_taxi_option()
+    urban_routes_page.click_call_taxi_button()
+    urban_routes_page.click_supportive_option()
+    time.sleep(2)
+    assert urban_routes_page.get_supportive_option()
 
 def test_order_blanket_and_handkerchiefs(self):
-#Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_custom_option()
+    urban_routes_page.click_taxi_option()
+    urban_routes_page.click_call_taxi_button()
+    urban_routes_page.click_supportive_option()
+    time.sleep(2)
+    assert urban_routes_page.get_supportive_option()
+
 
 def test_order_2_ice_creams(self):
     number_of_ice_cream = 2
     for ice_cream in range(2):
-#Add in S8
-print("function created for set route")
-pass
+    self.driver.get(data.URBAN_ROUTES_URL)
+    urban_routes_page = UrbanRoutesPage(self.driver)
+    urban_routes_page.enter_from_location(data.ADDRESS_FROM)
+    urban_routes_page.enter_to_location(data.ADDRESS_TO)
+    urban_routes_page.click_custom_option()
+    urban_routes_page.click_taxi_option()
+    urban_routes_page.click_call_taxi_button()
+    urban_routes_page.click_supportive_option()
+    time.sleep(2)
+    assert urban_routes_page.get_supportive_option()
 
-def test_car_search_model_appears(self):
-#Add in S8
-print("function created for set route")
-pass
 
+@classmethod
+def teardown_class(cls):
+    cls.driver.quit()
